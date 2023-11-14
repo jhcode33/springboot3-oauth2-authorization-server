085af8e46248796fddeea5dd6f4b41e44d3e6db6
jhcode33 <jhcode33@gmail.com>
2023-11-14 15:17:55 +0900

    Init: Springboot3-Oauth2-Authorization-Server Project

---
+++ src/main/java/com/example/springboot3oauth2authorizationserver/Springboot3Oauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth2authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Oauth2AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3Oauth2AuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot3oauth2authorizationserver/Springboot3Oauth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth2authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3Oauth2AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
