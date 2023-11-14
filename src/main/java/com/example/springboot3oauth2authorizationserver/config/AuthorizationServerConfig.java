b4b842e82d70308f6576fbbaa83e7b01aedf0577
takhoyeong <xkrghdud123@naver.com>
2023-11-14 21:51:34 +0900

    [추가: Class AuthorizationServerConfig]

--- src/main/java/com/example/springboot3oauth2authorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/springboot3oauth2authorizationserver/config/AuthorizationServerConfig.java
@@ -22,6 +22,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
+/**
+ *
+ * @author gopang
+ */
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
 
