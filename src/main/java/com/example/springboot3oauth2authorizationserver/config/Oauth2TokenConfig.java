9dd2c029d759a1d7480d0ddb6afdbf2b299099e4
jhcode33 <jhcode33@gmail.com>
2023-11-16 17:22:26 +0900

    Feat: Add TokenGenerator

---
+++ src/main/java/com/example/springboot3oauth2authorizationserver/config/Oauth2TokenConfig.java
@@ -0,0 +1,56 @@
+package com.example.springboot3oauth2authorizationserver.config;
+
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.token.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class Oauth2TokenConfig {
+
+    private final JWKSource<SecurityContext> jwtSource;
+    private final JwtEncoder jwtEncoder;
+
+    /**
+     *
+     * @return
+     * @author jhcode33
+     * @Date 2023.11.16
+     */
+    @Bean
+    public OAuth2TokenGenerator<?> tokenGenerator() {
+        JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+        jwtGenerator.setJwtCustomizer(jwtCustomizer());
+        OAuth2AccessTokenGenerator accessTokenGenerator = new OAuth2AccessTokenGenerator();
+        OAuth2RefreshTokenGenerator refreshTokenGenerator = new OAuth2RefreshTokenGenerator();
+        return new DelegatingOAuth2TokenGenerator(
+                jwtGenerator, accessTokenGenerator, refreshTokenGenerator);
+    }
+
+    /**
+     * Token에 어떤 정보가 저장될지 정할 수 있다
+     * @return
+     */
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> {
+            JwsHeader.Builder headers = context.getJwsHeader();
+            JwtClaimsSet.Builder claims = context.getClaims();
+            if (context.getTokenType().equals(OAuth2TokenType.ACCESS_TOKEN)) {
+                // Customize headers/claims for access_token
+
+            } else if (context.getTokenType().getValue().equals(OidcParameterNames.ID_TOKEN)) {
+                // Customize headers/claims for id_token
+
+            }
+        };
+    }
+}
