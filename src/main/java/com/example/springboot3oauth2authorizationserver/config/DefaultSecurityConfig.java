89920794b96c24bf0cef088172099ceed1a32483
jhcode33 <jhcode33@gmail.com>
2023-11-18 18:42:47 +0900

    Feat: Add Logout And Revoke

--- src/main/java/com/example/springboot3oauth2authorizationserver/config/DefaultSecurityConfig.java
+++ src/main/java/com/example/springboot3oauth2authorizationserver/config/DefaultSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.springboot3oauth2authorizationserver.config;
 
 import com.example.springboot3oauth2authorizationserver.security.JpaUserDetailsManager;
+import com.example.springboot3oauth2authorizationserver.service.JpaRegisteredClientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -10,12 +12,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,25 +32,26 @@ import java.util.List;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class DefaultSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
     private final JpaUserDetailsManager jpaUserDetailsManager;
-
-    public DefaultSecurityConfig(PasswordEncoder passwordEncoder, JpaUserDetailsManager jpaUserDetailsManager) {
-        this.passwordEncoder = passwordEncoder;
-        this.jpaUserDetailsManager = jpaUserDetailsManager;
-    }
+//    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 // 모든 요청에 대해서 인증해야 함
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/connect/logout")).permitAll()
                         .anyRequest().authenticated()
                 )
                 // security에서 제공하는 기본 login 페이지를 활용
                 .formLogin(Customizer.withDefaults());
+
+//        http.logout(logout -> logout
+//                .logoutSuccessHandler(oidcLogoutSuccessHandler()));
         return http.build();
 
     }
@@ -58,7 +65,16 @@ public class DefaultSecurityConfig {
         return daoAuthenticationProvider;
     }
 
-    /* CORS */
+//    @Bean
+//    public LogoutSuccessHandler oidcLogoutSuccessHandler() {
+//        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+//                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+//        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8081/home");
+//
+//        return oidcLogoutSuccessHandler;
+//    }
+
+    //== CORS ==/
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
--- src/main/java/com/example/springboot3oauth2authorizationserver/repository/AuthorizationRepository.java
+++ src/main/java/com/example/springboot3oauth2authorizationserver/repository/AuthorizationRepository.java
@@ -14,7 +14,7 @@ public interface AuthorizationRepository extends JpaRepository<Authorization, St
     Optional<Authorization> findByAuthorizationCodeValue(String authorizationCode);
     Optional<Authorization> findByAccessTokenValue(String accessToken);
     Optional<Authorization> findByRefreshTokenValue(String refreshToken);
-//    Optional<Authorization> findByOidcIdTokenValue(String idToken);
+    Optional<Authorization> findByOidcIdTokenValue(String idToken);
 //    Optional<Authorization> findByUserCodeValue(String userCode);
 //    Optional<Authorization> findByDeviceCodeValue(String deviceCode);
 
@@ -22,8 +22,8 @@ public interface AuthorizationRepository extends JpaRepository<Authorization, St
     @Query("select a from Authorization a where a.state = :token" +
             " or a.authorizationCodeValue = :token" +
             " or a.accessTokenValue = :token" +
-            " or a.refreshTokenValue = :token"
-//            "+ or a.oidcIdTokenValue = :token" +
+            " or a.refreshTokenValue = :token" +
+            " or a.oidcIdTokenValue = :token"
 //            " or a.userCodeValue = :token" +
 //            " or a.deviceCodeValue = :token"
     )
