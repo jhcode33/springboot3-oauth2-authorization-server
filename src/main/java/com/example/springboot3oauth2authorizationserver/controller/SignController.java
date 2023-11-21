7dc2776c139dff1e190d21bc40324ee4083aa954
jhcode33 <jhcode33@gmail.com>
2023-11-22 02:07:23 +0900

    Chore: build

--- src/main/java/com/example/springboot3oauth2authorizationserver/controller/SignController.java
+++ src/main/java/com/example/springboot3oauth2authorizationserver/controller/SignController.java
@@ -1,15 +1,12 @@
 package com.example.springboot3oauth2authorizationserver.controller;
 
-import com.example.springboot3oauth2authorizationserver.config.DatabaseLoader;
 import com.example.springboot3oauth2authorizationserver.dto.JoinDto;
-import com.example.springboot3oauth2authorizationserver.entity.Role;
 import com.example.springboot3oauth2authorizationserver.entity.User;
-import com.example.springboot3oauth2authorizationserver.security.CustomUserPrincipal;
-import com.example.springboot3oauth2authorizationserver.security.JpaUserDetailsManager;
 import com.example.springboot3oauth2authorizationserver.service.JoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
